e76c4a2469530791e4d9a86be9a634274c3f8a0c
Michael A Mayer <mikemayer67@vmwishes.com>
2024-02-06 23:10:08 -0500

    revamped menubar

--- shortcode/js/survey.js
+++ shortcode/js/survey.js
@@ -8,13 +8,13 @@ function setup_menubar()
     var survey_top = ce.container.offset().top;
     var survey_height = ce.container.outerHeight();
     var survey_bottom = survey_top + survey_height;
-    var menubar_height = ce.menubar_box.outerHeight(); 
+    var menubar_height = ce.menubar.outerHeight(); 
     var scroll_top = jQuery(window).scrollTop() + page_top;
 
     if( scroll_top > survey_bottom - menubar_height ) {
       if(menubar_fixed) {
         menubar_fixed = false;
-        ce.menubar_box.css({
+        ce.menubar.css({
           'position':'absolute',
           'top':(survey_height-menubar_height) + 'px',
           'left':0,
@@ -26,7 +26,7 @@ function setup_menubar()
         var survey_left = ce.container.offset().left;
         var survey_width = ce.container.outerWidth();
         menubar_fixed = true;
-        ce.menubar_box.css({
+        ce.menubar.css({
           'position':'fixed',
           'top':page_top,
           'left':survey_left + 'px',
@@ -36,7 +36,7 @@ function setup_menubar()
     } else {
       if(menubar_fixed) {
         menubar_fixed = false;
-        ce.menubar_box.css({
+        ce.menubar.css({
           'position':'absolute',
           'top':0,
           'left':0,
@@ -50,7 +50,7 @@ function setup_menubar()
 function setup_elements()
 {
   ce.container = jQuery('#survey');
-  ce.menubar_box = ce.container.find('.menubar-box');
+  ce.menubar = ce.container.find('nav.menubar');
 
   ce.wpadminbar = jQuery('#wpadminbar');
   if(ce.wpadminbar) {
--- shortcode/survey/menubar.php
+++ shortcode/survey/menubar.php
@@ -12,50 +12,77 @@ wp_enqueue_style('tlc-ttsurvey-survey', plugin_url('shortcode/css/survey.css'));
 function add_survey_menubar($userid)
 {
   $survey = current_survey();
-  $survey_name = $survey->name();
+  $survey_name = $survey->name() . " Time & Talent Survey";
 
   $user = User::from_userid($userid);
+  $fullname = $user->fullname();
 
   $status = "Status";
 
-  echo "<div class='menubar-box'>";
-  echo "<div class='menubar'>";
-  echo "<div class='menuitem survey'>$survey_name Time & Talent Survey</div>";
-  echo "<div class='menuitem status'>$status</div>";
-
-  add_user_menu($user);
-
-  echo "</div>"; // menubar
-  add_profile_editor($user);
-  echo "</div>"; // menubar-box
-  echo "<div class='spacer'></div>";
-}
-
-function add_user_menu($user)
-{
-  $fullname = $user->fullname();
   $form_uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+  $icon_url = plugin_url('/img/icons8-down.png');
+?>
 
-  echo "<div class='menuitem user'>";
-  echo "<span>$fullname</span>";
-  echo "<div class='menu'>";
-  echo "<ul>";
-  echo "<li><label for='profile-editor-toggle'>Edit Profile</label></li>";
-  echo "<li><a href='$form_uri?logout=1'>Log Out</a></li>";
-  echo "</ul>";
-  echo "</div>"; // menu
-  echo "</div>"; // menuitem
+<nav class='menubar'>
+  <div class='menubar-item survey-name'><?=$survey_name?></div>
+  <div class='menubar-item status'><?=$status?></div>
+  <div class='menubar-item user'>
+  <button class='menu-btn user'><?=$fullname?><img src='<?=$icon_url?>'></button>
+    <div class='menu user'>
+        <a href='#' data-action='edit-profile'>Edit Profile</a>
+        <a href='<?=$form_uri?>?logout=1'>Log Out</a>
+    </div>
+  </div>
+</nav>
+
+<?php
 }
 
-function add_profile_editor($user)
-{
-  $fullname = $user->fullname();
-  $email = $user->email() ?? "";
-
-  echo "<input id='profile-editor-toggle' type='checkbox' class='toggle'>";
-
-  echo "<div class='profile-editor'>";
-  echo "<p>Profile Editor for $fullname</p>";
-  echo "<div><label for='profile-editor-toggle' class='cancel'>Cancel</label></div>";
-  echo "</div>";
-}
+//function add_survey_menubar($userid)
+//{
+//  $survey = current_survey();
+//  $survey_name = $survey->name();
+//
+//  $user = User::from_userid($userid);
+//
+//  $status = "Status";
+//
+//  echo "<div class='menubar-box'>";
+//  echo "<div class='menubar'>";
+//  echo "<div class='menubar-item survey'>$survey_name Time & Talent Survey</div>";
+//  echo "<div class='menubar-item status'>$status</div>";
+//
+//  add_user_menu($user);
+//
+//  echo "</div>"; // menubar
+//  add_profile_editor($user);
+//  echo "</div>"; // menubar-box
+//  echo "<div class='spacer'></div>";
+//}
+//
+//function add_user_menu($user)
+//{
+//  $fullname = $user->fullname();
+//  $form_uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+//
+//  echo "<div class='menubar-item user'>";
+//  echo "<span>$fullname</span>";
+//  echo "<div class='menu'>";
+//  echo "<ul>";
+//  echo "<li><a class='menu-item'>Edit Profile</a></li>";
+//  echo "<li><a href='$form_uri?logout=1'>Log Out</a></li>";
+//  echo "</ul>";
+//  echo "</div>"; // menu
+//  echo "</div>"; // menuitem
+//}
+//
+//function add_profile_editor($user)
+//{
+//  $fullname = $user->fullname();
+//  $email = $user->email() ?? "";
+//
+//  echo "<div class='profile-editor'>";
+//  echo "<p>Profile Editor for $fullname</p>";
+//  echo "<div><a class='edit-profile-cancel'>Cancel</a></div>";
+//  echo "</div>";
+//}
