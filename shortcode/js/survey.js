d2be10b315d51e36e82520d89619509fb2cc9d6b
Michael A Mayer <mikemayer67@vmwishes.com>
2024-02-10 09:45:37 -0500

    checkpoint

--- shortcode/js/survey.js
+++ shortcode/js/survey.js
@@ -2,50 +2,6 @@ var ce = {};
 var page_top = 0;
 var menubar_fixed = false;
 
-function setup_menubar()
-{
-  jQuery(window).on('scroll',function(e) {
-    var survey_top = ce.container.offset().top;
-    var survey_height = ce.container.outerHeight();
-    var survey_bottom = survey_top + survey_height;
-    var menubar_height = ce.menubar.outerHeight(); 
-    var scroll_top = jQuery(window).scrollTop() + page_top;
-
-    if( scroll_top > survey_bottom - menubar_height ) {
-      if(menubar_fixed) {
-        menubar_fixed = false;
-        ce.menubar.css({
-          'position':'absolute',
-          'top':(survey_height-menubar_height) + 'px',
-          'left':0,
-          'width':'100%',
-        });
-      }
-    } else if(scroll_top > survey_top ) {
-      if(!menubar_fixed) {
-        var survey_left = ce.container.offset().left;
-        var survey_width = ce.container.outerWidth();
-        menubar_fixed = true;
-        ce.menubar.css({
-          'position':'fixed',
-          'top':page_top,
-          'left':survey_left + 'px',
-          'width':survey_width + 'px',
-        });
-      }
-    } else {
-      if(menubar_fixed) {
-        menubar_fixed = false;
-        ce.menubar.css({
-          'position':'absolute',
-          'top':0,
-          'left':0,
-          'width':'100%',
-        });
-      }
-    }
-  });
-}
 
 function setup_user_profile()
 {
@@ -60,37 +16,131 @@ function setup_user_profile()
   });
 
   jQuery(window).on('scroll',function(e) {
-    if( ce.profile_modal.is(':visible') ) {
-      const survey_top = ce.container.offset().top;
-      const survey_width = ce.container.outerWidth();
-      const survey_bottom = survey_top + ce.container.outerHeight();
-      const menubar_top = ce.menubar.offset().top;
-      const menubar_bottom = menubar_top + ce.menubar.outerHeight();
-      const editor_top = menubar_bottom + 5;
-      const editor_bottom = editor_top + ce.profile_editor.outerHeight();
-      const editor_width = 0.8 * survey_width;
-
-      if( editor_bottom > survey_bottom - 5 ) {
-        ce.profile_editor.hide();
-      } else {
-        ce.profile_editor.show();
-        if( menubar_fixed ) {
-          ce.profile_editor.css( {
-            'position':'fixed',
-            'top':page_top + 35,
-            'width':editor_width,
-          });
-        } else {
-          ce.profile_editor.css( {
-            'position':'absolute',
-            'top': 35,
-            'width':editor_width,
-          });
-        }
-      }
-    }
   });
 }
+
+
+function handle_scroll(e)
+{
+  const survey_height = ce.container.outerHeight();
+  const survey_width = ce.container.outerWidth();
+  const survey_top = ce.container.offset().top;
+  const survey_left = ce.container.offset().left;
+  const survey_bottom = survey_top + survey_height;
+
+  const menubar_height = ce.menubar.outerHeight(); 
+
+  const scroll_top = jQuery(window).scrollTop() + page_top;
+
+  var menubar_top = 0;
+
+  // Menubar
+
+  if( scroll_top > survey_bottom - menubar_height ) {
+    menubar_top = survey_bottom - menubar_height;
+    if(menubar_fixed) {
+      menubar_fixed = false;
+      ce.menubar.css({
+        'position':'absolute',
+        'top':(survey_height-menubar_height) + 'px',
+        'left':0,
+        'width':'100%',
+      });
+    }
+  } else if(scroll_top > survey_top ) {
+    menubar_top = scroll_top;
+    if(!menubar_fixed) {
+      menubar_fixed = true;
+      ce.menubar.css({
+        'position':'fixed',
+        'top':page_top,
+        'left':survey_left + 'px',
+        'width':survey_width + 'px',
+      });
+    }
+  } else {
+    menubar_top = survey_top;
+    if(menubar_fixed) {
+      menubar_fixed = false;
+      ce.menubar.css({
+        'position':'absolute',
+        'top':0,
+        'left':0,
+        'width':'100%',
+      });
+    }
+  }
+
+  // Profile Editor
+
+  if( ! ce.profile_modal.is(':visible') ) { return; }
+ 
+  const menubar_bottom = menubar_top + menubar_height;
+
+  const editor_height = ce.profile_editor.outerHeight();
+  const editor_width = 0.8 * survey_width;
+
+  const editor_top = menubar_bottom + 5;
+  const editor_bottom = editor_top + editor_height;
+
+  if( editor_bottom > survey_bottom - 5 ) {
+    ce.profile_editor.hide();
+  } else {
+    ce.profile_editor.show();
+    if( menubar_fixed ) {
+      ce.profile_editor.css( {
+        'position':'fixed',
+        'top':page_top + 35,
+        'width':editor_width,
+      });
+    } else {
+      ce.profile_editor.css( {
+        'position':'absolute',
+        'top': 35,
+        'width':editor_width,
+      });
+    }
+  }
+  
+}
+
+function handle_resize(e)
+{
+  const survey_left = ce.container.offset().left;
+  const survey_width = ce.container.outerWidth();
+
+  // Menubar
+
+  if(menubar_fixed) {
+    ce.menubar.css({
+      'left':survey_left + 'px',
+      'width':survey_width + 'px',
+    });
+  } else {
+    ce.menubar.css({
+      'left':0,
+      'width':survey_width + 'px',
+    });
+  }
+
+  // Profile Editor
+
+  if( ce.profile_modal.is(':visible') )
+  {
+    ce.profile_editor.css({'width':0.8*survey_width});
+  }
+
+  page_top = 0;
+  ce.wpadminbar = jQuery('#wpadminbar');
+  if(ce.wpadminbar) {
+    if(ce.wpadminbar.css('position') == 'fixed') {
+      page_top = ce.wpadminbar.height();
+    }
+  }
+
+  handle_scroll(e);
+}
+
 
 function setup_elements()
 {
@@ -109,7 +159,9 @@ function setup_elements()
     }
   }
 
-  setup_menubar();
+  jQuery(window).on('scroll',handle_scroll);
+  jQuery(window).on('resize',handle_resize);
+
   setup_user_profile();
 }
 
