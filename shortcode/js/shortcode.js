8a119ca8be1e9cf202e5d4f8d67a493ffd255573
Michael A Mayer <mikemayer67@vmwishes.com>
2024-02-23 22:50:07 -0500

    removed in/out nfo trigger

--- shortcode/js/shortcode.js
+++ shortcode/js/shortcode.js
@@ -2,42 +2,24 @@
 
 function info_setup()
 {
-  var info_trigger_timer = null;
-
   jQuery('#tlc-ttsurvey .info-box').hide();
-
   jQuery('#tlc-ttsurvey .info-trigger').each(
     function() {
       var trigger = jQuery(this)
       var tgt_id = trigger.data('target');
       var tgt = jQuery('#'+tgt_id);
-      trigger.on('mouseenter', function(e) {
-        if(info_trigger_timer) { 
-          clearTimeout(info_trigger_timer); 
-          info_trigger_timer=null;
-        }
-        info_trigger_timer = setTimeout(function() {
-          tgt.slideDown(100)
-        }, 500);
-      });
-      trigger.on('mouseleave',function(e) {
-        if(info_trigger_timer) {
-          clearTimeout(info_trigger_timer); 
-          info_trigger_timer=null;
-        }
-        if(!tgt.hasClass('locked')) { 
-          tgt.slideUp(100) 
-        } 
-      });
       trigger.on( 'click', function() { 
-        if(tgt.hasClass('locked')) {
-          tgt.removeClass('locked').slideUp(100)
+        if(tgt.hasClass('visible')) {
+          tgt.removeClass('visible').slideUp(100)
         } else {
-          tgt.addClass('locked').slideDown(100)
+          jQuery('#tlc-ttsurvey .info-box').removeClass('visible').slideUp(100);
+          tgt.addClass('visible').slideDown(100);
         }
       });
 
-      tgt.on( 'click', function() { tgt.removeClass('locked').slideUp(100) });
+      tgt.on( 'click', function() { 
+        tgt.removeClass('visible').slideUp(100) ;
+      });
     }
   );
 }
