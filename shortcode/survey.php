88f7b3ffff4c2f8594ee21ca962b0e9f2b6d1f79
Michael A Mayer <mikemayer67@vmwishes.com>
2024-02-26 23:22:13 -0500

    split menubar.css from survey.css

--- shortcode/survey.php
+++ shortcode/survey.php
@@ -12,6 +12,7 @@ require_once plugin_path('include/logger.php');
 require_once plugin_path('shortcode/menubar.php');
 
 wp_enqueue_style('tlc-ttsurvey-survey', plugin_url('shortcode/css/survey.css'));
+wp_enqueue_style('tlc-ttsurvey-menubar', plugin_url('shortcode/css/menubar.css'));
 
 function add_survey_content($userid=null)
 {
